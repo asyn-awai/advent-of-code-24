a930bc4f540d04940dc2c79ee344436d8371b032
asyn-awai <bwabersmanschool@gmail.com>
2024-12-01 13:56:06 -0800

    add .gitignore

--- day-01/target/debug/deps/part1-f3df96ff7db91235.d
+++
@@ -1,6 +0,0 @@
-/home/m/Documents/code/advent-of-code/day-01/target/debug/deps/part1-f3df96ff7db91235: src/bin/part1.rs src/bin/./input.txt
-
-/home/m/Documents/code/advent-of-code/day-01/target/debug/deps/part1-f3df96ff7db91235.d: src/bin/part1.rs src/bin/./input.txt
-
-src/bin/part1.rs:
-src/bin/./input.txt:
--- day-01/target/debug/part2.d
+++
@@ -1,1 +0,0 @@
-/home/m/Documents/code/advent-of-code/day-01/target/debug/part2: /home/m/Documents/code/advent-of-code/day-01/src/bin/input.txt /home/m/Documents/code/advent-of-code/day-01/src/bin/part2.rs
